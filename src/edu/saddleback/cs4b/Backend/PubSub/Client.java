cee471211ec15c372354d29fa3d03d0c77f91cf8
adillon3 <andrewrobertdillon@gmail.com>
2020-04-07 17:50:45 -0700

    Client Constructor

--- src/edu/saddleback/cs4b/Backend/PubSub/Client.java
+++ src/edu/saddleback/cs4b/Backend/PubSub/Client.java
@@ -1,18 +1,47 @@
 package edu.saddleback.cs4b.Backend.PubSub;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
     private PacketSender sender;
     private PacketReceiver receiver;
     private Socket socket;
+    private int portNumber;
+    private String hostName;
     private Thread listeningThread;
     private ObjectInputStream in;
     private ObjectOutputStream out;
-    private int portNumber;
-    //controller
+
+    //controller?
 
 
+    public Client(String newHostName, int newPortNumber) {
+        sender   = new PacketSender();
+        receiver = new PacketReceiver();
+
+        //Establishing socket and IOStreams
+        portNumber = newPortNumber;
+        hostName   = newHostName;
+        createSocket();
+
+        //Creating the listeningThread
+        listeningThread = new Thread();
+        listeningThread.start();
+    }
+
+    private void createSocket()
+    {
+        try
+        {
+            socket = new Socket(hostName, portNumber);
+            out = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            out.flush();
+            in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
 }
