339f1fd658d8e70d834d42663513a8d6a513cb2c
adillon3 <andrewrobertdillon@gmail.com>
2020-04-07 18:36:08 -0700

    started thinking through the implementation of the update function in PacketReceiver

--- src/edu/saddleback/cs4b/Backend/PubSub/PacketReceiver.java
+++ src/edu/saddleback/cs4b/Backend/PubSub/PacketReceiver.java
@@ -3,6 +3,11 @@ package edu.saddleback.cs4b.Backend.PubSub;
 public class PacketReceiver implements UIObserver{
     @Override
     public void update(SystemEvent e) {
+        //This handles the inout and output of messages
+        //SystemEvent is an interface that wraps around message event which holds a base message
+        //Since base message is the base class, it can be any of the subclasses
+        //this function uses the SystemEvent class to indirectly access the base message
+        //This function then proceeds to handle each message appropriately
 
     }
 }
