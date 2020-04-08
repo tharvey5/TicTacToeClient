39b46282ff4f7e22766c4a61b8d07ac4ebbd5ffc
adillon3 <andrewrobertdillon@gmail.com>
2020-04-07 17:23:45 -0700

    Worked on private datamembers for Client

--- src/edu/saddleback/cs4b/Backend/PubSub/Client.java
+++ src/edu/saddleback/cs4b/Backend/PubSub/Client.java
@@ -1,4 +1,18 @@
 package edu.saddleback.cs4b.Backend.PubSub;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Client {
+    private PacketSender sender;
+    private PacketReceiver receiver;
+    private Socket socket;
+    private Thread listeningThread;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private int portNumber;
+    //controller
+
+
 }
