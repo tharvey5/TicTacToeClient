9d3fb24fb1c9e6ada3e2a1c90204e7b05b20d38e
adillon3 <andrewrobertdillon@gmail.com>
2020-04-09 15:58:30 -0700

    Worked on update method for packetsender

--- src/edu/saddleback/cs4b/Backend/ClientPackage/PacketSender.java
+++ src/edu/saddleback/cs4b/Backend/ClientPackage/PacketSender.java
@@ -12,8 +12,9 @@ import java.util.List;
 
 public class PacketSender implements UIObserver {
     private ObjectOutputStream out;
-    private List<UIObserver> observers;
     private UISubject subject;
+
+
 
     public PacketSender(ObjectOutputStream newOut, UISubject newSubject)
     {
@@ -25,6 +26,10 @@ public class PacketSender implements UIObserver {
     @Override
     public void update(SystemEvent e)
     {
+
+        //massive if else that calls appropriate method to send and or create functions
+
+
 
         /*
         public void update(Sendable data) {
@@ -87,7 +92,111 @@ public class PacketSender implements UIObserver {
     }
 
 
+/*
+public class ChatSender implements UIObserver {
 
+    private String username;
+
+
+
+
+
+    @Override
+    public void update(Sendable data) {
+        String type = data.getType();
+        if (type.equals(SendTypes.MESSAGE.getType()))
+        {
+            UIFields messageField = (UIFields)data;
+            if (messageField.getValue() instanceof TextMessage)
+            {
+                TextMessage message = (TextMessage) messageField.getValue();
+                focusedChannel = message.getChannel();
+                sendMessage(message);
+            }
+            else if (messageField.getValue() instanceof PicMessage)
+            {
+                sendPicture((PicMessage)messageField.getValue());
+            }
+        }
+        else if (type.equals(SendTypes.JOIN.getType()))
+        {
+            UIFields message = (UIFields)data;
+            RegMessage reg = (RegMessage)message.getValue();
+            register(reg);
+        }
+        else if (type.equals(SendTypes.CHANNEL.getType()))
+        {
+            UIFields message = (UIFields)data;
+            UpdateMessage um = (UpdateMessage)message.getValue();
+            updateChannels(um);
+        } else if (type.equals(SendTypes.LEAVE.getType()))
+        {
+            UIFields discon = (UIFields)data;
+            DisconnectMessage disMsg = (DisconnectMessage)discon.getValue();
+            try
+            {
+                out.writeObject(new Packet(MessageType.DISCONNECT.getType(), disMsg));
+                out.flush();
+            }
+            catch(IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+        else if (type.equals(SendTypes.HISTORY_REQUEST.getType()))
+        {
+            UIFields req = (UIFields)data;
+            RequestMessage historyRequest = (RequestMessage)req.getValue();
+            try
+            {
+                out.writeObject(new Packet(MessageType.HISTORY_REQUEST.getType(), historyRequest));
+                out.flush();
+            }
+            catch(IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    // repeat code will be later refactored to be more concise
+    private void updateChannels(UpdateMessage updateMessage)
+    {
+        try
+        {
+            out.writeObject(new Packet(MessageType.IMAGE.getType(), updateMessage));
+            out.flush();
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    private void register(RegMessage regMessage)
+    {
+        try
+        {
+            out.writeObject(new Packet(MessageType.REGISTRATION.getType(), regMessage));
+            out.flush();
+
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (NullPointerException ne)
+        {
+            // should figure out a way later to have this solely handled by controller
+            // better infrastructure in-place where this happens with sent message
+            Platform.runLater(()-> {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Server appears to be offline. Please exit and try again later");
+                alert.showAndWait();
+            });
+        }
+    }
+ */
 
 
 
