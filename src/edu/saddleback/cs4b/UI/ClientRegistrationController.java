7f1f2115f0d0ce277111493bb911902fcade7bb3
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-10 13:53:58 -0700

    pubsub for clientRegCtrl

--- src/edu/saddleback/cs4b/UI/ClientRegistrationController.java
+++ src/edu/saddleback/cs4b/UI/ClientRegistrationController.java
@@ -1,5 +1,7 @@
 package edu.saddleback.cs4b.UI;
 
+import edu.saddleback.cs4b.Backend.Messages.*;
+import edu.saddleback.cs4b.Backend.PubSub.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,15 +13,65 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class ClientRegistrationController
+public class ClientRegistrationController implements UISubject, Observer
 {
+    private List<UIObserver> uiObservers = new ArrayList<>();
+    private AbstractMessageFactory factory = MessageFactoryProducer.getFactory(FactoryTypes.ADMIN_FACT.getTypes());
+
     @FXML
     Button returnToLoginButton;
 
     @FXML
     Button registerAccountButton;
 
+    /**
+     * This will be called by the client backend
+     */
+    @Override
+    public void update(SystemEvent e)
+    {
+        if (e.getEvent().getType().equals(EventType.MESSAGE_EVENT.getType())) {
+            BaseMessage message = ((MessageEvent)e.getEvent()).getMessage();
+            handleMessageEvents(message);
+        }
+    }
+
+    private void handleMessageEvents(BaseMessage message) {
+        if (message instanceof SuccessfulRegistration) {
+            // call the register account scene
+        } else if (message instanceof RegistrationErrorMessage) {
+            // call the
+        }
+    }
+
+    /**
+     * The following will be called by this UI
+     */
+    @Override
+    public void addObserver(UIObserver o)
+    {
+        uiObservers.add(o);
+    }
+
+    @Override
+    public void removeObserver(UIObserver o)
+    {
+        uiObservers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers(SystemEvent e)
+    {
+        Iterator<UIObserver> iterator = uiObservers.iterator();
+        while (iterator.hasNext())
+        {
+            iterator.next().update(e);
+        }
+    }
 
     /**
      * WHEN THIS METHOD IS CALLED THE 'RETURN TO LOGIN' BUTTON WILL CHANGE COLOR WHEN THE MOUSE IS HOVERING OVER IT
@@ -62,27 +114,26 @@ public class ClientRegistrationController
     @FXML
     public void handleReturnToLoginAction(ActionEvent event) throws IOException
     {
-        Parent parent = FXMLLoader.load(getClass().getResource("/edu/saddleback/cs4b/UI/ClientLogin.fxml"));
-        Scene scene  = new Scene(parent);
-
-        // This line gets the Stage information
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-        window.setScene(scene);
-        window.show();
+        swapScene("/edu/saddleback/cs4b/UI/ClientLogin.fxml");
     }
 
     @FXML
     public void handleRegisterAccountAction(ActionEvent event) throws IOException
     {
-        Parent parent = FXMLLoader.load(getClass().getResource("/edu/saddleback/cs4b/UI/AccountCreationSuccessScreen.fxml"));
+        swapScene("/edu/saddleback/cs4b/UI/AccountCreationSuccessScreen.fxml");
+    }
+
+    public void swapScene(String sceneLocation)
+    {
+        Parent parent = null;
+        try {
+            parent = FXMLLoader.load(getClass().getResource(sceneLocation));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Scene scene  = new Scene(parent);
-
-        // This line gets the Stage information
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
+        Stage window = (Stage)(returnToLoginButton).getScene().getWindow();
         window.setScene(scene);
         window.show();
     }
-
 }
