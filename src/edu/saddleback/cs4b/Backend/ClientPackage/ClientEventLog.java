f94e91e46bae885eebb5c9b70e72ad4460d5522d
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-13 10:17:04 -0600

    client event log init

---
+++ src/edu/saddleback/cs4b/Backend/ClientPackage/ClientEventLog.java
@@ -0,0 +1,22 @@
+package edu.saddleback.cs4b.Backend.ClientPackage;
+
+import edu.saddleback.cs4b.Backend.PubSub.Observer;
+import edu.saddleback.cs4b.Backend.PubSub.Subject;
+import edu.saddleback.cs4b.Backend.PubSub.SystemEvent;
+
+public class ClientEventLog implements Subject {
+    @Override
+    public void addObserver(Observer o) {
+
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+
+    }
+
+    @Override
+    public void notifyObserver(SystemEvent e) {
+
+    }
+}
