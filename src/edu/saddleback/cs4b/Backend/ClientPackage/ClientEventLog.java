a377dfc9939547a4543ce638b2c6eb5eab9796ad
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-13 10:20:05 -0600

    add subject methods to log

--- src/edu/saddleback/cs4b/Backend/ClientPackage/ClientEventLog.java
+++ src/edu/saddleback/cs4b/Backend/ClientPackage/ClientEventLog.java
@@ -3,20 +3,29 @@ package edu.saddleback.cs4b.Backend.ClientPackage;
 import edu.saddleback.cs4b.Backend.PubSub.Observer;
 import edu.saddleback.cs4b.Backend.PubSub.Subject;
 import edu.saddleback.cs4b.Backend.PubSub.SystemEvent;
+import edu.saddleback.cs4b.Backend.PubSub.UIObserver;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class ClientEventLog implements Subject {
+    private List<Observer> observers;
+
     @Override
     public void addObserver(Observer o) {
-
+        observers.add(o);
     }
 
     @Override
     public void removeObserver(Observer o) {
-
+        observers.remove(o);
     }
 
     @Override
     public void notifyObserver(SystemEvent e) {
-
+        Iterator<Observer> iterator = observers.iterator();
+        while (iterator.hasNext()) {
+            iterator.next().update(e);
+        }
     }
 }
