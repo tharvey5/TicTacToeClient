83f4bcdbeb3d1841bdbc47a5094715b6981bb77d
Jeff Adams <jeffreyadams2566@gmail.com>
2020-04-13 22:10:04 -0700

    Added new Profile page for users to view their profile

--- src/edu/saddleback/cs4b/UI/ClientLoginController.java
+++ src/edu/saddleback/cs4b/UI/ClientLoginController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -151,13 +152,18 @@ public class ClientLoginController implements Observer
         }
     }
 
+    public void handleForgotPasswordAction(MouseEvent event)
+    {
+        swapScene("/edu/saddleback/cs4b/UI/ForgotPassword.fxml");
+    }
+
     public void swapToHome()
     {
         swapScene("/edu/saddleback/cs4b/UI/ClientHome.fxml");
     }
 
     @FXML
-    public void handleCreateAccountAction(ActionEvent event)
+    public void handleCreateAccountAction(MouseEvent event)
     {
         swapScene("/edu/saddleback/cs4b/UI/ClientRegistration.fxml");
     }
--- src/edu/saddleback/cs4b/UI/ViewProfileController.java
+++ src/edu/saddleback/cs4b/UI/ViewProfileController.java
@@ -1,6 +1,33 @@
 package edu.saddleback.cs4b.UI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+
 public class ViewProfileController
 {
+    @FXML
+    Button editProfileButton;
+
+    /**
+     * WHEN THIS METHOD IS CALLED THE 'CREATE ACCOUNT' BUTTON WILL CHANGE COLOR WHEN THE MOUSE IS HOVERING OVER IT
+     */
+    @FXML
+    public void highlightEditProfile()
+    {
+        editProfileButton.setOnMouseEntered(mouseEvent -> editProfileButton.setTextFill(Color.YELLOW));
+    }
+
+    /**
+     * WHEN THIS METHOD IS CALLED THE 'CREATE ACCOUNT' BUTTON WILL CHANGE BACK TO THE DEFAULT TEXT COLOR WHEN THE MOUSE IS
+     * NO LONGER HOVERING OVER IT
+     */
+    @FXML
+    public void resetEditProfile()
+    {
+        editProfileButton.setOnMouseExited(mouseEvent -> editProfileButton.setTextFill(Color.WHITE));
+    }
+
+
 
 }
