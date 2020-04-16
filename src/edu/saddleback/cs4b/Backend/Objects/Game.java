bff539e034faa024c12b06540f5458b0f5458569
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-15 23:42:43 -0600

    message dependencies included

---
+++ src/edu/saddleback/cs4b/Backend/Objects/Game.java
@@ -0,0 +1,29 @@
+package edu.saddleback.cs4b.Backend.Objects;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface Game
+{
+    public LocalDateTime getStartTime();
+    public void setStartTime(LocalDateTime newTime);
+
+    public String getStartPlayer();
+    public void setStartPlayer(String newStartPlayer);
+
+    public String getOtherPlayer();
+    public void setOtherPlayer(String newOtherPlayer);
+
+    public List<Move> getMoves();
+    public void setMoves(List<Move> newMoves);
+    public void addMove(Move newMove);
+
+    public String getWinner();
+
+    public String getGameID();
+    public void setGameID(String newGameID);
+
+    public Board getGameBoard();
+    public void setGameBoard(Board newBoard);
+
+}
