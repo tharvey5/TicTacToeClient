5a16581988ef53e4108c14f13a28431fe4cdeddb
Tucker Harvey <coopert.harvey@gmail.com>
2020-04-21 20:19:44 -0600

    get and set tokens

--- src/edu/saddleback/cs4b/Backend/Objects/Game.java
+++ src/edu/saddleback/cs4b/Backend/Objects/Game.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public interface Game
 {
+    void setToken(Token token, PublicUser user);
+    Token getToken(PublicUser user);
+
     String getStartTime();
     void setStartTime(LocalDateTime newTime);
 
