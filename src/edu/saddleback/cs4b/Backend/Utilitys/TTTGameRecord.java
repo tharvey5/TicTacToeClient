2bb6e2a9e3e2417721a10922bbf0ffb6641cc062
adillon3 <andrewrobertdillon@gmail.com>
2020-04-23 16:26:07 -0700

    Began working on TTTGameRecord, finished the GameRecord Funtions, still need constructors etc.

--- src/edu/saddleback/cs4b/Backend/Utilitys/TTTGameRecord.java
+++ src/edu/saddleback/cs4b/Backend/Utilitys/TTTGameRecord.java
@@ -7,42 +7,54 @@ import java.util.List;
 
 public class TTTGameRecord implements GameRecord, Serializable
 {
+    List<Game> gameRecord;
+    int wins;
+    int losses;
+
+    CONSTRUCTORS!!!!!!!!
+
+
+
+
+
+
+
+
+
+
+
     public List<Game> getGameRecord()
     {
-
+        return gameRecord;
     }
     public void setGameRecord(List<Game> newGameRecord)
     {
-
+        gameRecord = newGameRecord;
     }
 
     public int  getNumGames()
     {
-
+        return gameRecord.size();
     }
-
     public int  getWins()
     {
-
+        return wins;
     }
     public void setWins(int newWins)
     {
-
+        wins = newWins;
     }
-
     public int  getLosses()
     {
-
+        return losses;
     }
-
     public void setLosses(int newLosses)
     {
-
+        losses = newLosses;
     }
-
-
     public int  getTies()
     {
-
+        int total = getNumGames();
+        return total - wins - losses;
     }
 }
