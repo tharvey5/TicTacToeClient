7cd87a4a72104042aa904922288f8e0757036c57
adillon3 <andrewrobertdillon@gmail.com>
2020-04-25 13:14:38 -0700

    Finished SetTokenMessage and updated MsgTypes enum

--- src/edu/saddleback/cs4b/Backend/Messages/SetTokenMessage.java
+++ src/edu/saddleback/cs4b/Backend/Messages/SetTokenMessage.java
@@ -1,4 +1,36 @@
 package edu.saddleback.cs4b.Backend.Messages;
 
-public class SetTokenMessage {
+import edu.saddleback.cs4b.Backend.Objects.Token;
+
+import java.util.Set;
+
+public class SetTokenMessage extends BaseMessage
+{
+    private Token token;
+
+    public SetTokenMessage()
+    {
+        this(null);
+    }
+
+    public SetTokenMessage(Token newToken)
+    {
+        super(MsgTypes.SET_TOKEN);
+        token = newToken;
+    }
+
+    public Token getToken()
+    {
+        return token;
+    }
+
+    public String getTokenID()
+    {
+        return token.getTokenID();
+    }
+
+    public void setToken(Token newToken)
+    {
+        token = newToken;
+    }
 }
