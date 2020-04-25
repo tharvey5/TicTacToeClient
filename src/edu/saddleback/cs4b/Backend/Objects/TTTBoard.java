d0630e73f5e2681e4adb2fbae15f58d190a52a70
adillon3 <andrewrobertdillon@gmail.com>
2020-04-24 22:27:11 -0700

    Continued working on TTTBoard class

--- src/edu/saddleback/cs4b/Backend/Objects/TTTBoard.java
+++ src/edu/saddleback/cs4b/Backend/Objects/TTTBoard.java
@@ -5,33 +5,43 @@ import java.io.Serializable;
 public class TTTBoard implements Board, Serializable
 {
 
+    String[][] board = new String[3][3];
+
+
+
+
+
+
+
     @Override
-    public void setToken(int r, int c, Token token) {
-
-    }
-
-    @Override
-    public void setToken(Coordinate coordinate, Token token) {
-
+    public Token getToken(int r, int c)
+    {
+        return board[r][c];
     }
 
     @Override
     public Board getBoard() {
-        return null;
+        return this;
     }
-
-    @Override
-    public Token getToken(int r, int c) {
-        return null;
-    }
-
     @Override
     public int rows() {
-        return 0;
+        return board.length;
     }
-
     @Override
     public int cols() {
-        return 0;
+        return board[0].length;
+    }
+    @Override
+    public void setToken(int r, int c, Token token)
+    {
+        board[r][c] = token.getTokenID();
+    }
+    @Override
+    public void setToken(Coordinate coordinate, Token token)
+    {
+        int x = coordinate.getXCoord();
+        int y = coordinate.getYCoord();
+
+        setToken(x, y, token);
     }
 }
