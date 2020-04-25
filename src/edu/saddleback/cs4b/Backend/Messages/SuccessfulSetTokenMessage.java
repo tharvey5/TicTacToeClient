654e9bbbec7600e7dc1ca77e6d47fe4902461f29
adillon3 <andrewrobertdillon@gmail.com>
2020-04-25 13:17:48 -0700

    Created successfulSetTokenMessage

--- src/edu/saddleback/cs4b/Backend/Messages/SuccessfulSetTokenMessage.java
+++ src/edu/saddleback/cs4b/Backend/Messages/SuccessfulSetTokenMessage.java
@@ -1,4 +1,9 @@
 package edu.saddleback.cs4b.Backend.Messages;
 
-public class SuccessfulSetTokenMessage37.	SetTokenErrorMessage  {
+public class SuccessfulSetTokenMessage extends BaseMessage
+{
+    public SuccessfulSetTokenMessage()
+    {
+        super(MsgTypes.SUCCESS_SET_TOKEN);
+    }
 }
