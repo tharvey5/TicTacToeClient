aa90e79a7f8cb414bf252787b9c7282130dd2a65
adillon3 <andrewrobertdillon@gmail.com>
2020-04-24 21:52:32 -0700

    Completed implementing Move methods into TTTMove class

--- src/edu/saddleback/cs4b/Backend/Objects/TTTMove.java
+++ src/edu/saddleback/cs4b/Backend/Objects/TTTMove.java
@@ -4,52 +4,51 @@ import java.time.LocalDateTime;
 
 public class TTTMove implements Move
 {
+    private String gameID;
+    private String playerID;
+    private Coordinate coordinate;
+    private LocalDateTime moveTime;
+
+
 
     @Override
     public String getGameID()
     {
-
+        return gameID;
     }
-
     @Override
     public void setGameID(String newGameID)
     {
-
+        gameID = newGameID;
     }
-
     @Override
     public LocalDateTime getStartTime()
     {
-        return null;
+        return moveTime;
     }
-
     @Override
     public void setStartTime(LocalDateTime time)
     {
-
+        moveTime = time;
     }
-
     @Override
     public String getPlayerID()
     {
-
+        return playerID;
     }
-
     @Override
     public void setPlayerID(String newPlayerID)
     {
-
+        playerID = newPlayerID;
     }
-
     @Override
     public Coordinate getCoordinate()
     {
-
+        return coordinate;
     }
-
     @Override
     public void setCoordinate(Coordinate newCoordinate)
     {
-
+        coordinate = newCoordinate;
     }
 }
