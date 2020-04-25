d82905e5d2b7eb746a162a5e54796481829e6da0
adillon3 <andrewrobertdillon@gmail.com>
2020-04-24 22:00:41 -0700

    Created TTTMove class

--- src/edu/saddleback/cs4b/Backend/Objects/TTTMove.java
+++ src/edu/saddleback/cs4b/Backend/Objects/TTTMove.java
@@ -9,7 +9,26 @@ public class TTTMove implements Move
     private Coordinate coordinate;
     private LocalDateTime moveTime;
 
+    public TTTMove()
+    {
+        this(null, null, null);
+    }
 
+    public TTTMove(String newGameID, String newPlayerID, Coordinate newCoordinate)
+    {
+        gameID = newGameID;
+        playerID = newPlayerID;
+        coordinate = newCoordinate;
+        moveTime   = LocalDateTime.now();
+    }
+    public TTTMove(String newGameID, String newPlayerID, int newX, int newY)
+    {
+        this(newGameID, newPlayerID, new TTTPosition(newX, newY));
+    }
+    public void setMoveTimeAsNow()
+    {
+        moveTime   = LocalDateTime.now();
+    }
 
     @Override
     public String getGameID()
