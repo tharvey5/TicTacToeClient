214d8b9018c2c49a790ef02058d20cc7826dc11e
adillon3 <andrewrobertdillon@gmail.com>
2020-04-24 21:43:41 -0700

    Added TTTBoard and TTTMove classes, NOT FILLED OUT

---
+++ src/edu/saddleback/cs4b/Backend/Objects/TTTMove.java
@@ -0,0 +1,55 @@
+package edu.saddleback.cs4b.Backend.Objects;
+
+import java.time.LocalDateTime;
+
+public class TTTMove implements Move
+{
+
+    @Override
+    public String getGameID()
+    {
+
+    }
+
+    @Override
+    public void setGameID(String newGameID)
+    {
+
+    }
+
+    @Override
+    public LocalDateTime getStartTime()
+    {
+        return null;
+    }
+
+    @Override
+    public void setStartTime(LocalDateTime time)
+    {
+
+    }
+
+    @Override
+    public String getPlayerID()
+    {
+
+    }
+
+    @Override
+    public void setPlayerID(String newPlayerID)
+    {
+
+    }
+
+    @Override
+    public Coordinate getCoordinate()
+    {
+
+    }
+
+    @Override
+    public void setCoordinate(Coordinate newCoordinate)
+    {
+
+    }
+}
