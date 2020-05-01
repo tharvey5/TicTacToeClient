40accc83315b86fcfd0fb82076b5c8c3eb8fea87
Tucker Harvey <coopert.harvey@gmail.com>
2020-05-01 11:49:38 -0700

    init AI runner

---
+++ src/edu/saddleback/cs4b/Backend/ClientPackage/ClientAIRunner.java
@@ -0,0 +1,10 @@
+package edu.saddleback.cs4b.Backend.ClientPackage;
+
+/**
+ * This class will be used to run the AI for users who wish to play a
+ * single player game. It will sign into the system using its own
+ * universal login
+ */
+public final class ClientAIRunner
+{
+}
