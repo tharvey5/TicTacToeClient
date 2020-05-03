281e2499bcf97c56037b67f1b272ebf2f2cdaa96
Tucker Harvey <coopert.harvey@gmail.com>
2020-05-02 19:17:17 -0700

    created ai event log

--- src/edu/saddleback/cs4b/Main.java
+++ src/edu/saddleback/cs4b/Main.java
@@ -1,5 +1,6 @@
 package edu.saddleback.cs4b;
 
+import edu.saddleback.cs4b.Backend.ClientPackage.AIEventLog;
 import edu.saddleback.cs4b.Backend.ClientPackage.Client;
 import edu.saddleback.cs4b.Backend.ClientPackage.ClientAIRunner;
 import edu.saddleback.cs4b.Backend.ClientPackage.ClientEventLog;
@@ -21,6 +22,7 @@ public class Main extends Application {
 
         new Thread(()->{
             Client client = new Client("localhost", 8080, UIEventLog.getInstance(), ClientEventLog.getInstance());
+            Client aiConnect = new Client("localhost", 8080, AIEventLog.getInstance(), )
         }).start();
 
         //new Thread(ClientAIRunner.getInstance()).start();
