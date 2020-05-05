3eb801502643ecf3c8c4d02d72ddd17e60921851
Tucker Harvey <coopert.harvey@gmail.com>
2020-05-04 21:54:22 -0700

    updated profile message

--- src/edu/saddleback/cs4b/Backend/Messages/AuthenticatedMessage.java
+++ src/edu/saddleback/cs4b/Backend/Messages/AuthenticatedMessage.java
@@ -1,5 +1,6 @@
 package edu.saddleback.cs4b.Backend.Messages;
 
+import edu.saddleback.cs4b.Backend.Utilitys.Profile;
 import edu.saddleback.cs4b.Backend.Utilitys.User;
 
 /**
@@ -8,11 +9,20 @@ import edu.saddleback.cs4b.Backend.Utilitys.User;
  */
 public class AuthenticatedMessage extends BaseMessage {
     private User authUser;
+    private Profile profile;
 
     public AuthenticatedMessage() { super(MsgTypes.AUTHENTICATION);}
+
 
     public User getAuthUser() { return authUser; }
     public void setAuthUser(User user) {
         this.authUser = user;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+    public Profile getProfile() {
+        return profile;
     }
 }
